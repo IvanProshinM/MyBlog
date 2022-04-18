7b74a643424d30fa4eeefc2424b72e011aaa9524
IvanProshinM <ivan.proshin.m@gmail.com>
2022-04-18 12:49:23 +0300

    Первый комит

---
+++ src/config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mariadb;dbname=mailsender',
+    'username' => 'mailsender',
+    'password' => 'qwerty',
+    'charset' => 'utf8',
+
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ src/controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class AuthController extends Controller
+{
+  public function actionRegistration
+
+
+
+
+
+
+
+
+}
